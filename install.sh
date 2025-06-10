5672854b25e8f8e286f3247081bde804a315b87f
Kevyn Andrade <kevsdeandrade@gmail.com>
2025-06-10 10:28:31 -0300

    removendo trecho desnecessário, adicionando docker

--- install.sh
+++ install.sh
@@ -26,8 +26,13 @@ sudo apt-get install -y \
   git git-core \
   wine \
   playonlinux \
-  dconf-editor
+  dconf-editor \
+  chromium-browser \
+  docker docker-compose
 
+
+
+wget https://code.visualstudio.com/docs/?dv=linux64_deb; 
 # Headphone Bluetooth
 sudo apt-get install blueman pulseaudio-module-bluetooth
 pactl load-module module-bluetooth-discover
@@ -36,38 +41,12 @@ echo "After that, open sound settings and select your device."
 echo "On 'profile' entry, choose 'High Fidelity Playback' and we're done."
 
 if has_not pip; then
-  sudo apt-get install python-pip python-dev build-essential
+  sudo apt-get install python-pip python3-dev build-essential
   sudo pip install --upgrade pip 
   sudo pip install --upgrade virtualenv
 fi
 ok "pip"
 
-if has_not chromium-browser; then
-  sudo apt install chromium-browser
-fi
-ok "Chrome"
-
-if has_not docker; then
-  sudo apt-get install apt-transport-https ca-certificates linux-image-extra-$(uname -r) -y
-  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -sc) main' | cat > /etc/apt/sources.list.d/docker.list"
-  sudo apt-get update
-  sudo apt-get purge lxc-docker
-  sudo apt-get install docker-engine -y
-  sudo service docker start
-  sudo groupadd docker
-  sudo usermod -aG docker $(id -un)
-fi
-ok "Docker"
-
-
-if has_not diffmerge; then
-  wget -O diffmerge.deb http://download-us.sourcegear.com/DiffMerge/4.2.0/diffmerge_4.2.0.697.stable_amd64.deb
-  sudo dpkg -i diffmerge.deb --ignore-depends
-  sudo apt-get install -fy
-  rm -rf diffmerge.deb
-fi
-ok "Diffmerge"
 
 if ! [[ -d "$HOME/.oh-my-zsh" ]]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
